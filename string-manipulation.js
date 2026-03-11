b21f87623a0f25ebbf3c53ef2d3ea771bc69b7a6
Nathan Grace <favouramaraci1@gmail.com>
2026-03-11 11:20:09 +0100

    feat: string-manipulation

---
+++ string-manipulation.js
@@ -0,0 +1,8 @@
+const fullName="Nathan Amarachi"
+const lenghtofName = fullName.length
+const nameUpper = fullName.toUpperCase()
+const nameLower = fullName.toLowerCase()
+
+console.log(lenghtofName)
+console.log(nameUpper)
+console.log(nameLower)
